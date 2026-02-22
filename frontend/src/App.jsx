9e5ad77a7bd11d7c1daa312798cac2e8dc33446d
ankita mishra <ankitamishra8763@gmail.com>
2026-02-22 13:57:10 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { lazy, Suspense } from "react";
 import "./App.css";
 
-import Dashboard from "./pages/Dashboard";
-import TeacherDetails from "./pages/TeacherDetails";
+// Lazy load
+const Dashboard = lazy(() => import("./pages/Dashboard"));
+const TeacherDetails = lazy(() => import("./pages/TeacherDetails"));
 
 function App() {
   return (
     <Router>
       <div className="app-container">
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/teacher/:id" element={<TeacherDetails />} />
-        </Routes>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/teacher/:id" element={<TeacherDetails />} />
+          </Routes>
+        </Suspense>
       </div>
     </Router>
   );
